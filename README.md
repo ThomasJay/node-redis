e1cedbb4d8f4b39fbb4dacc6590acbf1532ecdce
Thomas Jay <thomasjay200@gmail.com>
2020-05-28 20:23:29 -0700

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # node-redis
+
 Usage of Redis in Node
+
+We are using async-redis so we can use async/await rather then callbacks as supported by the base redis code.
+
+We will connect to a redis server.
+
+We have a call GET /cached/{key} that will return the value for a key or a not found error
+
+We have a call POST /cached/{key}/{value} that will set the value for a key without any timeout.
+
+We have a call POST /cached/{key}/{value}/{timeout} that will set the value for a key WITH a timeout.
+
+We have a call DELETE /cached/{key} that will remove the value for a key.
---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const asyncRedis = require("async-redis");
+
+// Connect to redis server
+const client = asyncRedis.createClient("redis://localhost:6379");
+
+const app = express();
+
+// Support json post body
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const port = 3000;
+
+// Default get
+app.get("/", (req, res) => res.send("Hello Async Redis Test"));
+
+app.get("/cached/:key", async (req, res) => {
+  const key = req.params.key;
+
+  const value = await client.get(key);
+
+  if (value) {
+    console.log("key=" + key + " value=" + value);
+    res.status(200).send(value);
+  } else {
+    console.log("key=" + key + " Not found");
+    res.status(400).send("Not found");
+  }
+});
+
+app.post("/cached/:key/:value", async (req, res) => {
+  const key = req.params.key;
+  const value = req.params.value;
+
+  await client.set(key, value);
+
+  console.log("Set key=" + key + " value=" + value);
+
+  res.status(200).send({ [key]: value });
+});
+
+app.post("/cached/:key/:value/:timeout", async (req, res) => {
+  const key = req.params.key;
+  const value = req.params.value;
+  const timeout = req.params.timeout;
+
+  await client.set(key, value, "EX", parseInt(timeout));
+
+  console.log("Set key=" + key + " value=" + value);
+
+  res.status(200).send({ [key]: value });
+});
+
+app.delete("/cached/:key", async (req, res) => {
+  const key = req.params.key;
+
+  await client.del(key);
+
+  console.log("Del key=" + key);
+
+  res.status(200).send("removed");
+});
+
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
